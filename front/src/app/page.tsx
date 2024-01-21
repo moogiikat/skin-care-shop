a3ae21ed0d8ff2e7d9fdc83fa56d151522e26179
nominnnnn <s.nomin@gtn.co.jp>
2024-01-21 19:35:07 +0800

    arag ys home

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,27 +3,281 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-5">
-      <p>Дорнод оригинал гоо сайхны бүтээгдэхүүн</p>
-      <Link href="/suggestions">
-        <button className="btn">
-          Button
+    <div className="">
+      <div className="flex flex-col items-center gap-5 text-center pb-5">
+        <div className="flex flex-row z-20 sticky top-0 bg-purple-200 py-2 px-4 w-full font-semibold text-purple-950">
+          <div className="pl-4 py-3 h-fit w-24">
+            <img src="assets/images/dan-but/logo.png" alt="logo" />
+          </div>
+          <div className="flex flex-col  bg-purple-200 py-4 w-full font-semibold text-purple-950">
+            Дорнод Оргинал Гоо Сайхны Бүтээгдэхүүн
+            <p>Dornod Original Skin Care Products</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-fit lg:flex-row gap-5">
+          <div className="">
+            <Link href="/suggestions">
+              <button className="btn  hover:-translate-y-1 hover:scale-110 duration-100 bg-pink-200 hover:bg-pink-300">
+                Арьсны асуудлаар сонгох
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    className=" stroke-pink-800"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </button>
+            </Link>
+          </div>
+          <div className="divider lg:divider-horizontal">эсвэл</div>
+          <div className="">
+            <Link href="/suggestions">
+              <button className="btn  hover:-translate-y-1 hover:scale-110 duration-100 bg-pink-200 hover:bg-pink-300">
+                Бүтээгдэхүүний төрлөөр сонгох
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    className=" stroke-pink-800"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <p className="font-bold text-xl">Онцлох бүтээгдэхүүн</p>
+        <div className="carousel carousel-center max-w-full gap-4 pt-4">
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/body-skin-care/cubilose_hand_cream.png"
+                alt="hand_cream"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h3 className="card-title text-base">
+                Шувууны үүрний охьтой гарын тос
+              </h3>
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/masks/goat_night.png"
+                alt="masks"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">
+                Ямааны сүүний охьтой шөнийн маск
+              </h2>
+
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="indicator">
+            <span className="indicator-item indicator-center badge badge-primary h-8 text-white">
+              Шинэ бүтээгдэхүүн
+            </span>
+            <div className="card carousel-item w-60 bg-base-100 ">
+              <figure className="px-4 pt-4">
+                <img
+                  src="assets/images/dan-but/wheat_cleanser_new.png"
+                  alt="body_lotion"
+                  className="rounded-xl"
+                />
+              </figure>
+              <div className="card-body p-4 items-center text-center">
+                <h2 className="card-title text-base">
+                  Улаан буудайн охьтой нүүр угаагч
+                </h2>
+                <div className="card-actions">
+                  <button className="btn btn-primary text-white">
+                    Дэлгэрэнгүй
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/masks/tea_mask.png"
+                alt="masks"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">Модны цайны хандтай маск</h2>
+
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <p className="font-bold text-xl pt-5">Хямдралтай бүтээгдэхүүн</p>
+        <div className="carousel carousel-center max-w-full gap-4">
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/dan-but/lip_balm.png"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">
+                Улаан буудайн охьтой уруулын бальзам
+              </h2>
+              <div className="flex items-center gap-4 font-sans">
+                <div className="line-through  text-base">10'000₮</div>
+                <div className=" text-red-500 font-semibold ">8'000₮</div>
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/set/soybean_set.png"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">
+                Буурцагны үрийн охьтой сэт
+              </h2>
+              <div className="flex items-center gap-4 font-sans">
+                <div className="line-through  text-base">250'000₮</div>
+                <div className=" text-red-500 font-semibold ">220'000₮</div>
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/body-skin-care/avocado_lotion.png"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">
+                Авокадо жимсний охьтой биеийн тос
+              </h2>
+              <div className="flex items-center gap-4 font-sans">
+                <div className="line-through  text-base">32'000₮</div>
+                <div className=" text-red-500 font-semibold ">28'000₮</div>
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card carousel-item w-60 bg-base-100 ">
+            <figure className="px-4 pt-4">
+              <img
+                src="assets/images/dan-but/wheat_cleanser.png"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body p-4 items-center text-center">
+              <h2 className="card-title text-base">
+                Улаан буудайн охьтой нүүр цэвэрлэгч
+              </h2>
+
+              <div className="flex items-center gap-4 font-sans">
+                <div className="line-through  text-base">50'000₮</div>
+                <div className=" text-red-500 font-semibold ">40'000₮</div>
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-primary text-white">
+                  Дэлгэрэнгүй
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+        <aside className="items-center grid-flow-col">
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
+            width="36"
+            height="36"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            className="fill-current"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
+            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
           </svg>
-        </button>
-      </Link>
+          <p>Copyright © 2024 - All right reserved</p>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.560 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
+        </nav>
+      </footer>
     </div>
   );
 }
